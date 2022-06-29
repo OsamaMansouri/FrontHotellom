ee92202f19c5229d05342ea436608752d51562fc
oussama-siscoo <oussama.aitboulaha@gmail.com>
2022-06-29 18:24:11 +0100

    update axios instance

--- src/@core/api/axiosInstance.js
+++ src/@core/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //http://127.0.0.1:8000
 const axiosInstance = axios.create({
-  baseURL: 'https://api.hotellom.com'
+  baseURL: 'http://127.0.0.1:8000'
 })
 
 export default axiosInstance

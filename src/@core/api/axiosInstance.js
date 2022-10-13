24b6d4dd0ef8fdf7c6254009811229364bd5ae39
Osama Mansouri <mansouri.osama@gmail.com>
2022-10-13 17:13:43 +0100

    fix request name fot

--- src/@core/api/axiosInstance.js
+++ src/@core/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //http://127.0.0.1:8000  000
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000'
+  baseURL: 'http://localhost:8000/'
 })
 
 export default axiosInstance
